8f892f4fee9a58e118e4587887c6cae72d740729
Naig <eng_gian@hotmail.com>
2026-05-18 05:42:56 +0200

    Add Light Module Draft V0

---
+++ project-files/archive/module_drafts/light_controller.py
@@ -0,0 +1,15 @@
+# My-co-Lab
+# Light Module Draft V0
+
+def light_on():
+    print("Light ON")
+
+def light_off():
+    print("Light OFF")
+
+def test():
+    light_on()
+    light_off()
+
+if __name__ == "__main__":
+    test()
