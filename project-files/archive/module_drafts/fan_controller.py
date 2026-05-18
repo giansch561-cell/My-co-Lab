7700b48b79fa3309a4b37b2aa73b4df98cfaa088
Naig <eng_gian@hotmail.com>
2026-05-18 05:29:01 +0200

    Add Fan Module Draft V0

---
+++ project-files/archive/module_drafts/fan_controller.py
@@ -0,0 +1,16 @@
+# My-co-Lab
+# Fan Module Draft V0
+
+def fan_on():
+    print("Fan ON")
+
+def fan_off():
+    print("Fan OFF")
+
+def test():
+    fan_on()
+    fan_off()
+
+if __name__ == "__main__":
+    test()
+    
