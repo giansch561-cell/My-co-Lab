08c7dd3b54a8dc5ab5a500ab8e8a7e0c8dc55573
Naig <eng_gian@hotmail.com>
2026-05-18 05:32:04 +0200

    Add Camera Module Draft V0

--- project-files/archive/module_drafts/camera_capture.py
+++ project-files/archive/module_drafts/camera_capture.py
@@ -1,8 +1,12 @@
 # My-co-Lab
-# Camera Module Draft
+# Camera Module Draft V0
+
+from datetime import datetime
 
 def capture_image():
-    print("Capture image")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(f"[{timestamp}] Capture image")
 
 if __name__ == "__main__":
     capture_image()
