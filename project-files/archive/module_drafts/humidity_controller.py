37c0d167e395d931257644de34e869bcdda74392
Naig <eng_gian@hotmail.com>
2026-05-18 05:36:04 +0200

    Add Humidity Module Draft V0

---
+++ project-files/archive/module_drafts/humidity_controller.py
@@ -0,0 +1,15 @@
+# My-co-Lab
+# Humidity Module Draft V0
+
+def humidifier_on():
+    print("Humidifier ON")
+
+def humidifier_off():
+    print("Humidifier OFF")
+
+def test():
+    humidifier_on()
+    humidifier_off()
+
+if __name__ == "__main__":
+    test()
